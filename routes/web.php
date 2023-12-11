2d76ad4378070a7f7a56b4aab042e0cb935dc030
JasmanSu <Suiqqwan@gmail.com>
2023-12-11 16:31:44 +0800

    episode 8

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/' ,function () {
+    return view ('posts');
+});
+
+
+Route::get('post' ,function () {
+    return view ('post', [
+        'post' => '<h1>Hello World</h1>'
+    ]);
 });
